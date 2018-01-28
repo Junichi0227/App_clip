a39da502944b638a5354369b3a7b824a73bb729e
Junichi0227 <araiguma.227@gmail.com>
2018-01-28 16:23:52 +0900

    ジョイスティック実装（仮）

---
+++ App_Climb/Unity/App_clip/Assets/Scripts/MovingCube.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingCube : MonoBehaviour
+{
+
+    //先ほど作成したJoystick
+    [SerializeField]
+    private Joystick _joystick = null;
+
+    //移動速度
+    private const float SPEED = 0.5f;
+
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+
+        pos.x += _joystick.Position.x * SPEED;
+        pos.y += _joystick.Position.y * SPEED;
+
+        transform.position = pos;
+    }
+
+}
