58a1e6b11e8f5494c6a1b0d0df792d657d4adbf2
Junichi0227 <araiguma.227@gmail.com>
2018-01-28 14:45:48 +0900

    first commit

---
+++ App_Climb/Unity/App_clip/Assets/Scripts/HammerPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerPosition : MonoBehaviour {
+
+    public GameObject LHam,RHam,LHand,RHand;
+	
+	// Update is called once per frame
+	void Update () {
+        LHand.transform.position = LHam.transform.position;
+        RHand.transform.position = RHam.transform.position;
+	}
+}
